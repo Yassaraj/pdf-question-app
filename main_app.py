e669bbc1d5620d72525bd90b90bc05e1a9bc765f
Yassaraj <86739831+Yassaraj@users.noreply.github.com>
2024-12-09 01:00:13 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -9,6 +9,8 @@ import faiss
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from uuid import uuid4
+import getpass
+
 
 # Load environment variables
 load_dotenv()
@@ -29,19 +31,29 @@ if uploaded_file is not None:
     data = loader.load()
 
     # Show the document details
-    st.write(f'You have {len(data)} document(s) in your data.')
-    st.write(f'There are {len(data[0].page_content)} characters in your sample document.')
-    st.write(f'Here is a sample: {data[0].page_content[:200]}')
+    # st.write(f'You have {len(data)} document(s) in your data.')
+    # st.write(f'There are {len(data[0].page_content)} characters in your sample document.')
+    # st.write(f'Here is a sample: {data[0].page_content[:200]}')
 
     # Split the document into chunks
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=50)
     texts = text_splitter.split_documents(data)
 
     # Show the number of chunks
-    st.write(f'Now you have {len(texts)} documents (chunks).')
+    #st.write(f'Now you have {len(texts)} documents (chunks).')
 
     # Set up the embeddings and vector store
-    genai.configure(api_key="AIzaSyDzg1YuwkBsU_whJvghcsnMVRliKtAZVSY")
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"] 
+
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content("Explain how AI works")
+
+    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    if "GOOGLE_API_KEY" not in os.environ:
+        os.environ["GOOGLE_API_KEY"] = getpass(GOOGLE_API_KEY)
+
+
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
 
@@ -60,11 +72,11 @@ if uploaded_file is not None:
     if query:
         # Retrieve relevant documents from the vector store
         results = vector_store.similarity_search(query, k=5)
-        st.write("Relevant documents:")
-        for res in results:
-            st.write(f"* {res.page_content} [{res.metadata}]")
+        # st.write("Relevant documents:")
+        # for res in results:
+        #     st.write(f"* {res.page_content} [{res.metadata}]")
         
-        # Generate the answer using Google Generative AI
+        #Generate the answer using Google Generative AI
         response = genai.GenerativeModel("gemini-1.5-flash").generate_content(
             f"For the given query:{query}, generate a short answer based on the results:{results}. If the relevant information is not available, return as non without making things up."
         )
